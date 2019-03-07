2b323fa572525478eee62b34042f581b505edbcb
Mary West <westmarye1219@gmail.com>
2019-03-06 20:33:01 -0600

    console.log working

--- projects.js
+++ projects.js
@@ -47,33 +47,36 @@ const projects = [
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
   }
-]
+];
 
   const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML += textToPrint
+    selectedDiv.innerHTML = textToPrint
 }
 
 const createProjectCards = () => {
     let domString = '';
     for(let i = 0; i < projects.length; i++) {
-    domString += `<div class = 'projects'>`
+    domString += `<div class = 'projectsPage'>`
     domString += `<h3>${projects[i].title}</h3> `
-    domString += `<h3>${projects[i].screenshot}</h3> `
+    domString += `<img>${projects[i].screenshot}</img> `
     domString += `<h3>${projects[i].description}<h3>`
     domString += `<h3>${projects[i].technologiesUsed}<h3>`
     domString += `<h3>${projects[i].available}<h3>`
     domString += `<h3>${projects[i].url}<h3>`
     domString += `<h3>${projects[i].githubUrl}<h3>`
     domString += `</div>`;
+    // console.log(projects[i].title);
     if (projects[i].available === true) {
         printToDom(domString, 'projectsPage');
         }
-
-
 }
-console.log('projects', domString);
-
+// console.log(domString);
 };
 
+const init = () => {
+    createProjectCards();
+}
+
+init();
 
